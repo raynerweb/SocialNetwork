a35ce92370fe5b0be92b55ce672caedf3ac73e58
rayner <raynerweb@gmail.com>
2021-06-03 16:32:39 -0300

    FormTableViewController

---
+++ SocialNetwork/FormTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  FormViewController.swift
+//  SocialNetwork
+//
+//  Created by rayner on 03/06/21.
+//
+
+import UIKit
+
+class FormTableViewController: UITableViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwdTextField: UITextField!
+    
+    @IBOutlet weak var checkPasswdTextField: UITextField!
+    
+    // Todos os campos acima entao ligados à este array
+    @IBOutlet var textFields: [UITextField]! {
+        didSet {
+            textFields.forEach{ textFields in
+                textFields.delegate = self
+            }
+        }
+    }
+    
+}
+
+
+// Comportamento que pula o cursor de TextField em TextField.
+// Quando nao 'retorno' for acionado. Entao fecha o teclado com resignFirstResponder() e nenum outro campo é o firstResponder
+extension FormTableViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if textField == nameTextField {
+            emailTextField.becomeFirstResponder()
+        }
+        if textField == emailTextField {
+            passwdTextField.becomeFirstResponder()
+        }
+        if textField == passwdTextField {
+            checkPasswdTextField.becomeFirstResponder()
+        }
+        return true
+    }
+    
+}
