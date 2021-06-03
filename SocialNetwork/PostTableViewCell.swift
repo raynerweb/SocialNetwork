d483b6e481b1c829e4a4b987518d23c10c2de468
rayner <raynerweb@gmail.com>
2021-06-03 18:39:53 -0300

    add SocialTableViewController

--- SocialNetwork/PostTableViewCell.swift
+++ SocialNetwork/PostTableViewCell.swift
@@ -8,7 +8,38 @@
 import UIKit
 
 class PostTableViewCell: UITableViewCell {
+    
+    static let kReuseIdentifier = "PostTableViewCell"
 
-
+    @IBOutlet weak var profilePictureImageView: UIImageView!
+    
+    @IBOutlet weak var profileName: UILabel!
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    @IBAction func followButton(_ sender: Any) {
+    }
+    
+    @IBAction func moreButton(_ sender: Any) {
+    }
+    
+    @IBAction func likeButton(_ sender: Any) {
+    }
+    
+    @IBAction func commentButton(_ sender: Any) {
+    }
+    
+    @IBAction func shareButton(_ sender: Any) {
+    }
+    
+    
+    static func register(inside tableView: UITableView) {
+        let nib = UINib(nibName: kReuseIdentifier, bundle: Bundle(for: PostTableViewCell.self))
+        tableView.register(nib, forCellReuseIdentifier: kReuseIdentifier)
+    }
+    
+    func setup(with data: String) {
+        profileName.text = data
+    }
     
 }
---
+++ SocialNetwork/SocialTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SocialTableViewController.swift
+//  SocialNetwork
+//
+//  Created by rayner on 03/06/21.
+//
+
+import UIKit
+
+class SocialTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        
+    }
+
+}
