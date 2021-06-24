1b8972222139c6e18910e95ba629099d77135caf
rayner <raynerweb@gmail.com>
2021-06-24 18:27:46 -0300

    recuperando dados da FetchedResultsController

--- SocialNetwork/Extensions/UITableViewController+FetchedResultsControllerDelegate.swift
+++ SocialNetwork/Extensions/UITableViewController+FetchedResultsControllerDelegate.swift
@@ -1,63 +1,70 @@
+////
+////  TableViewController+FetchedResultsControllerDelegate.swift
+////  SocialNetwork
+////
+////  Created by rayner on 20/06/21.
+////
 //
-//  TableViewController+FetchedResultsControllerDelegate.swift
-//  SocialNetwork
+//import Foundation
+//import CoreData
+//import UIKit
 //
-//  Created by rayner on 20/06/21.
+//extension UITableViewController: NSFetchedResultsControllerDelegate {
 //
+//    public func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+//        tableView.beginUpdates()
+//    }
+//
+//    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+//                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+//                    atSectionIndex sectionIndex: Int,
+//                    for type: NSFetchedResultsChangeType) {
+//        switch type {
+//        case .insert:
+//            tableView.insertSections([sectionIndex], with: .bottom)
+//        case .delete:
+//            tableView.deleteSections([sectionIndex], with: .bottom)
+//        case .update:
+//            tableView.reloadSections([sectionIndex], with: .automatic)
+//        default:
+//            break
+//        }
+//    }
+//
+//    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+//                    didChange anObject: Any,
+//                    at indexPath: IndexPath?,
+//                    for type: NSFetchedResultsChangeType,
+//                    newIndexPath: IndexPath?) {
+//
+//        if let index = indexPath {
+//            switch type {
+//            case .insert:
+//                tableView.insertRows(at: [index], with: .left)
+//            case .delete:
+//                tableView.deleteRows(at: [index], with: .left)
+//            case .update:
+//                tableView.reloadRows(at: [index], with: .middle)
+//            case .move:
+//                tableView.moveRow(at: index, to: newIndexPath!)
+//            @unknown default:
+//                break
+//            }
+//        }
+//
+//
+//    }
+//
+//    public func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+//        tableView.endUpdates()
+//    }
+//
+//
+//}
 
-import Foundation
-import CoreData
-import UIKit
-
-extension UITableViewController: NSFetchedResultsControllerDelegate {
-    
-    public func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.beginUpdates()
-    }
-     
-    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
-                    didChange sectionInfo: NSFetchedResultsSectionInfo,
-                    atSectionIndex sectionIndex: Int,
-                    for type: NSFetchedResultsChangeType) {
-        switch type {
-        case .insert:
-            tableView.insertSections([sectionIndex], with: .bottom)
-        case .delete:
-            tableView.deleteSections([sectionIndex], with: .bottom)
-        case .update:
-            tableView.reloadSections([sectionIndex], with: .automatic)
-        default:
-            break
-        }
-    }
-    
-    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
-                    didChange anObject: Any,
-                    at indexPath: IndexPath?,
-                    for type: NSFetchedResultsChangeType,
-                    newIndexPath: IndexPath?) {
-        
-        if let index = indexPath {
-            switch type {
-            case .insert:
-                tableView.insertRows(at: [index], with: .left)
-            case .delete:
-                tableView.deleteRows(at: [index], with: .left)
-            case .update:
-                tableView.reloadRows(at: [index], with: .middle)
-            case .move:
-                tableView.moveRow(at: index, to: newIndexPath!)
-            @unknown default:
-                break
-            }
-        }
-
-        
-    }
-    
-    public func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        tableView.endUpdates()
-    }
-    
-    
-}
+//import CoreData
+//import UIKit
+//
+//extension UserTableViewController : NSFetchedResultsControllerDelegate {
+//    
+//}
--- SocialNetwork/UserTableViewController.swift
+++ SocialNetwork/UserTableViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreData
 
-class UserTableViewController: UITableViewController {
+class UserTableViewController: UITableViewController , NSFetchedResultsControllerDelegate {
     
     private lazy var fetchRequest: NSFetchRequest<UserCoreData> = {
         let request: NSFetchRequest<UserCoreData> = UserCoreData.fetchRequest()
@@ -29,11 +29,12 @@ class UserTableViewController: UITableViewController {
 
     private let kBaseURL = "https://jsonplaceholder.typicode.com"
     
-    private var users = [User]() {
-        didSet {
-            tableView.reloadData()
-        }
-    }
+    private var users = [User]()
+//    {
+//        didSet {
+//            tableView.reloadData()
+//        }
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,6 +54,9 @@ class UserTableViewController: UITableViewController {
                 if users.isEmpty {
                     getUsersFromAPI()
                 }
+//                else {
+//                    self.users = users.map { $0.toUser() }
+//                }
             }
         }catch {
             debugPrint(error)
@@ -60,6 +64,8 @@ class UserTableViewController: UITableViewController {
     }
     
     private func getUsersFromAPI() {
+        let context = AppDelegate.viewContext
+        
         if let url = URL(string: "\(kBaseURL)/users") {
             let session = URLSession.shared
             let request = URLRequest(url: url)
@@ -69,20 +75,23 @@ class UserTableViewController: UITableViewController {
                     
                     if let users = try? JSONDecoder().decode([User].self, from: data!) {
                         DispatchQueue.main.async {
-                            self.users = users
-                            var userCoreDataList: [UserCoreData] = []
                             
+                            
+                            var userCoreDataList: [UserCoreData] = []
+
                             for user in users {
                                 userCoreDataList.append(user.toUserCoreData())
                             }
-                            
-                            let context = AppDelegate.viewContext
+
                             do {
                                 try context.save()
                                 print("Success")
                             } catch {
                                 print("Error saving: \(error)")
                             }
+                            
+                            self.users = users
+                            
                         }
                     }
                     
@@ -99,14 +108,14 @@ class UserTableViewController: UITableViewController {
 //        return fetchedResultsController.sections?.count ?? 1
 //    }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fetchedResultsController.fetchedObjects?.count ?? 0
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return fetchedResultsController.fetchedObjects?.count ?? 0
 //        if let sections = fetchedResultsController.sections, sections.count > 0 {
 //            return sections[section].numberOfObjects
 //        } else {
 //            return 0
 //        }
-    }
+//    }
 
 //    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 //        return self.users.count
@@ -152,5 +161,89 @@ class UserTableViewController: UITableViewController {
             }
         }
     }
+    
+    
+//    controller
+    
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+//        tableView.reloadData()
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+//        table
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch (type) {
+        case .insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break;
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break;
+        case .update:
+            if let indexPath = indexPath, let cell = tableView.cellForRow(at: indexPath) as? UserTableViewCell {
+                let userCoreData = fetchedResultsController.object(at: indexPath)
+                cell.user = userCoreData.toUser()
+            }
+            break;
+        case .move:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+
+            if let newIndexPath = newIndexPath {
+                tableView.insertRows(at: [newIndexPath], with: .fade)
+            }
+            break;
+        }
+    }
+
+    
+//    public func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+//        tableView.beginUpdates()
+//    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let sections = self.fetchedResultsController.sections else {
+            fatalError("No sections in fetchedResultsController")
+        }
+        let sectionInfo = sections[section]
+        return sectionInfo.numberOfObjects
+    }
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = <#Get the cell#>
+//        guard let object = self.fetchedResultsController?.object(at: indexPath) else {
+//            fatalError("Attempt to configure cell without a managed object")
+//        }
+//        // Configure the cell with data from the managed object.
+//        return cell
+//    }
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        guard let sectionInfo = fetchedResultsController?.sections?[section] else {
+//            return nil
+//        }
+//        return sectionInfo.name
+//    }
+//    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+//        return fetchedResultsController?.sectionIndexTitles
+//    }
+//    override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+//        guard let result = fetchedResultsController?.section(forSectionIndexTitle: title, at: index) else {
+//            fatalError("Unable to locate section for \(title) at index: \(index)")
+//        }
+//        return result
+//    }
 
 }
